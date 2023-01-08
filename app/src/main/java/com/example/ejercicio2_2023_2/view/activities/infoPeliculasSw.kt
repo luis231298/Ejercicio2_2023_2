eed8208117d0627381f35aac3fdf46c33edf0545
José Luis Luna <pepeluna23@yahoo.com>
2023-01-08 09:35:35 -0600

    corrección de error por culpa de color

--- app/src/main/java/com/example/ejercicio2_2023_2/view/activities/infoPeliculasSw.kt
+++ app/src/main/java/com/example/ejercicio2_2023_2/view/activities/infoPeliculasSw.kt
@@ -2,6 +2,8 @@ package com.example.ejercicio2_2023_2.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.ejercicio2_2023_2.databinding.ActivityInfoPeliculasSwBinding
 import com.example.ejercicio2_2023_2.databinding.ActivityInfoPersoSwBinding
 
@@ -13,5 +15,13 @@ class infoPeliculasSw : AppCompatActivity() {
         binding = ActivityInfoPeliculasSwBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val bundles = intent.extras
+        val pelis = bundles?.getStringArrayList("pelis")
+
+        Toast.makeText(this@infoPeliculasSw,pelis?.size.toString(), Toast.LENGTH_LONG).show()
+
+        //Glide.with(this@infoPeliculasSw).load(pelis?.get(0).toString()).into(binding.rvPelis)
+
     }
 }
